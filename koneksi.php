a7340b8cf27abaf4d1969b02305dbaefc8ca19f5
YudhPtra <yudhaptradw12@gmail.com>
2025-11-23 16:51:55 +0700

    update: koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,13 @@
 <?php
-// Konfigurasi Database
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "sekolah_db";
 
-// Melakukan koneksi ke MySQL
+$host       = "sql300.infinityfree.com"; 
+$user       = "if0_40208473";        
+$password   = "9R7UNP86JpQc";              
+$database   = "if0_40208473_w012_db";    
+
 $connect = mysqli_connect($host, $user, $password, $database);
 
-// Cek koneksi
 if (!$connect) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
